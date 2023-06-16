7b3bc9a8859c12a7131e79b4f7de1e608fea52da
Abdullah Al-khalidy <abdo33bado@gmail.com>
2023-06-17 02:03:44 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar';
+import './components/Navcss.css';
+import Sec from './components/secondpage/Sec';
+import './components/secondpage/Sec.css';
+function App() {
+  return (
+    <div className="App">
+    <>
+    <Navbar />
+    <Sec />
+    </>
+    </div>
+  );
+}
+
+export default App;
